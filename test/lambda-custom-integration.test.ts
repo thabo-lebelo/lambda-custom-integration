cefec897cd6b0df88829c34d6a0dc6a8c5a34f67
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-23 12:18:02 +0200

    Lambda custom integration in API Gateway

---
+++ test/lambda-custom-integration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaCustomIntegration from '../lib/lambda-custom-integration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-custom-integration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaCustomIntegration.LambdaCustomIntegrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
