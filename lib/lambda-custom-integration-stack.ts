9aa05abbb8e25e6ea5809ab46a9661cd14c24d19
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-23 23:32:16 +0200

    Custom integration in API Gateway

--- lib/lambda-custom-integration-stack.ts
+++ lib/lambda-custom-integration-stack.ts
@@ -1,16 +1,31 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as gateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
+/**
+ * A stack for our simple Lambda-powered web service
+ */
 export class LambdaCustomIntegrationStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The Lambda function that contains the functionality
+        const backend = new lambda.Function(this, 'Lambda', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'index.handler',
+            code: lambda.Code.fromAsset(path.join(__dirname, '/../src/')),
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCustomIntegrationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // An API Gateway to make the Lambda web-accessible
+        const api = new gateway.LambdaRestApi(this, 'Gateway', {
+            description: 'Endpoint for a simple Lambda-powered web service',
+            handler: backend,
+            deployOptions: {
+                stageName: "dev"
+            },
+        });
+
+    }
 }
